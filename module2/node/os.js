d8321d6a524f25d4ae9b3cc6123e02479791bad1
Rishu-cell <rishu1010.cse19@chitkara.edu.in>
2022-03-23 22:47:25 +0530

    os file created

---
+++ module2/node/os.js
@@ -0,0 +1,10 @@
+let os=require("os");
+//console.log(os);
+
+console.log(os.hostname()); //gives the host name
+
+console.log(os.arch()); //architecture
+
+console.log(os.networkInterfaces());
+
+console.log(os.userInfo());
