2db992aab406a1e46017c6854e1d88222c681090
Rishu-cell <rishu1010.cse19@chitkara.edu.in>
2022-03-23 17:11:19 +0530

    Module2 created

---
+++ module2/intro.js
@@ -0,0 +1,32 @@
+console.log("Hello Guys");
+
+console.log("Pepcoders");
+
+//variable declaration
+
+let a=10; //numeric
+console.log(a);
+
+let b='a'; //char
+console.log(b);
+
+let c='I am a string'; //string
+console.log(c);
+
+let d=true;     //boolean value
+console.log(d);
+
+//loops
+//for loop
+for(let i=1;i<=5;i++)
+{
+    console.log(i);
+}
+//while loop
+let count=1;
+while(count<=5)
+{
+    console.log(count);
+    count++;
+}
+
