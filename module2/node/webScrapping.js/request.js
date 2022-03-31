98578a29aa0785394c2fe324c24163a1e838ba9a
Rishu-cell <rishu1010.cse19@chitkara.edu.in>
2022-04-01 00:13:20 +0530

    bastsman details file created

--- module2/node/webScrapping.js/request.js
+++ module2/node/webScrapping.js/request.js
@@ -13,3 +13,18 @@ request('http://www.google.com',function(error,response,body){
 });
 
 // function m function call kr skte hai /*CALL BACK  agr koi fun as a parameter pass hota hai function me */
+
+
+/*
+const request = require('request');
+
+request('http://www.google.com', cb);
+
+function cb(error, response, body) {
+    if(error)
+        console.error('error:', error); // Print the error if one occurred
+    else{
+        console.log('body:', body); // Print the HTML for the Google homepage.
+    }
+}
+*/
