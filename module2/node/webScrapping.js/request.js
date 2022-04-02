0fa3e4efb03445f869b83c6af1ac6f7c7196de14
Rishu-cell <rishu1010.cse19@chitkara.edu.in>
2022-04-02 22:27:59 +0530

    async file created to show how waiting queue call stack and node api works

--- module2/node/webScrapping.js/request.js
+++ module2/node/webScrapping.js/request.js
@@ -1,8 +1,8 @@
 /*WHAT IS WEB SCRAPPING ?? 
-WEB SE data ko uthana ..................
+WEB SE kuj particular data ko uthana ..................
  */
 // npm i request   [install kri request // i means install]
-const request=require('request');
+const request=require('request');  
 request('http://www.google.com',function(error,response,body){
     if(error){
         console.error('error:',error) // print the error if one ocured
