93d9a4853556b7f6e21cab0ed941986d14db3746
Rishu-cell <rishu1010.cse19@chitkara.edu.in>
2022-04-16 19:02:18 +0530

    callback hell

--- module2/node/webScrapping.js/callback.js
+++ module2/node/webScrapping.js/callback.js
@@ -37,3 +37,31 @@ function cb (error,data)
 }
 console.log("after")*/
 // error kb aayega jb file ka name glt diya hoga
+
+
+//this is callback hell when yu want to read files in a particular order i.e first the file1 then file 2 even though
+//the file 1 is of bigger size than file 2
+
+/*
+const fs = require("fs");
+
+console.log("Before ");
+
+fs.readFile("file.txt", function (error,data){
+    if(error){
+        console.log(error);
+    }else{
+        console.log(data+"");
+        fs.readFile("f2.txt",function(error,data){
+            if(error){
+                console.log(error);
+            }else{
+                console.log(data+"");
+            }
+        });
+    }
+});
+
+console.log("After");
+
+*/
